a317a57ac42736643b285b84620c9050f6320a54
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-06 10:53:47 +0600

    mission accomplished 🔥🚀🔥

--- week_1/day_6/A_Helpful_Maths.cpp
+++ week_1/day_6/A_Helpful_Maths.cpp
@@ -7,7 +7,6 @@ int main(){
     string s;cin>>s;
     vector<int>v;
     for(int i=0;i<s.size();i+=2)v.push_back(s[i]-'0');
-    // for(int i=0;i<s.size();i++)cout<<v[i]<<" ";
     sort(v.begin(),v.end());
     for(int i=0;i<v.size()-1;i++){
         cout<<v[i]<<"+";
