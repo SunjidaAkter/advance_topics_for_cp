84b81ca6db5531939e487c2ab797fb9ad9183d8c
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-16 12:24:21 +0600

    mission accomplished 🔥🚀🔥

--- week_3/day_2/C_Premutation.cpp
+++ week_3/day_2/C_Premutation.cpp
@@ -12,25 +12,27 @@ using namespace std;
 typedef pair<int,int>pii;
 #define forl(ty,var,str,end) for(ty var=str; var<end; var++)
 # define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
-// bool cmp(map<pii,int>a,map<pii,int>b){
-//     if(a)
-// }
 void solve(){
     int n;cin>>n;
-    int a[n][n];
-    map<pii,int>mp;
+    vector<vector<int>>v(n,vector<int>(n-1));
+    map<int,vector<int>>mp;
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>a[i][j];
-            mp[{a[i][j],j+1}]++;
+        for(int j=0;j<n-1;j++){
+            cin>>v[i][j];
+            mp[v[i][j]].push_back(j+1);
         }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>a[i][j];
+    vector<int>f(n);
+    for(auto &it:mp){
+        sort(it.second.begin(),it.second.end(),greater<int>());
+        if(it.second[0]<=n-2)f[it.second[0]-1]=it.first;
+        else{
+            if(it.second[0]==it.second[it.second.size()-1])f[n-1]=it.first;
+            else f[n-2]=it.first;
         }
     }
-
+    for(int val:f)cout<<val<<" ";
+    cout<<nl;
 }
 int main(){
     FAST;
