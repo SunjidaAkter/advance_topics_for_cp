f1d760276a72b2808c679cac805d3b66076d350f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-18 14:37:18 +0600

    mission accomplished 🔥🚀🔥

--- week_3/day_5/S_Count_the_Number_of_Pairs.cpp
+++ week_3/day_5/S_Count_the_Number_of_Pairs.cpp
@@ -24,43 +24,26 @@ void solve(){
     }
     int s1=0,sum=0;
     for(int i=0;i<26;i++){
-        // cout<<fr1[i]<<" "<<fr2[i]<<nl;
         if(fr1[i]>0||fr2[i]>0){
             if(fr1[i]==0){
-                // cout<<s1<<"a"<<nl;
                 if(fr2[i]>1)s1+=fr2[i]/2;
             }
             if(fr2[i]==0){
-                // cout<<s1<<"b"<<nl;
                 if(fr1[i]>1)s1+=fr1[i]/2;
             }
             if(fr1[i]-fr2[i]==0)sum+=fr1[i];
             else{
                 if(fr1[i]-fr2[i]>0)sum+=fr2[i];
                 if(fr1[i]-fr2[i]<0)sum+=fr1[i];
-                if((fr1[i]>1&&fr2[i]>1)&&abs(fr1[i]-fr2[i])%2==0){
-                    // cout<<s1<<"c"<<nl;
+                if((fr1[i]>0&&fr2[i]>0)&&(abs(fr1[i]-fr2[i])>1)){
                     s1+=(abs(fr1[i]-fr2[i])/2);
                 }
-                else if((fr1[i]>0&&fr2[i]>0)&&abs(fr1[i]-fr2[i])%2==1){
-                    // cout<<s1<<"d"<<nl;
-                    s1+=((abs(fr1[i]-fr2[i])/2)+1);
-                }
-
             }
         }
     }
-    // c(s1);
     if(k>=s1)sum+=s1;
     else sum+=k;
     c(sum);
-    // set<char>st;
-    // for(int i=0;i<s.size();i++)st.insert(s[i]);
-    // while (!st.empty()){
-    //     cout<<*st.begin()<<" ";
-    //     st.erase(st.begin());
-    // }
-    // cout<<nl;
 }
 int main(){
     FAST;
