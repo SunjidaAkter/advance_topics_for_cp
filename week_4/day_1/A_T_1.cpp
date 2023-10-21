9dcc3481a6f4dc14c9270d9a36700c3e1ba9b726
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-21 18:22:18 +0600

    mission accomplished 🔥🚀🔥

--- week_4/day_1/A_T_1.cpp
+++ week_4/day_1/A_T_1.cpp
@@ -4,22 +4,26 @@ using namespace std;
 typedef pair<ll,ll>pii;
 #define forl(var,str,end) for(long long int var=str; var<end; var++)
 int main(){
-    int n;cin>>n;
-    int a[n];
-    int sum=0;
-    for(int i=0;i<n;i++)cin>>a[i];
-    for(int i=0;i<n;i++)sum+=a[i];
+    ll n;cin>>n;
+    ll a[n];
+    ll sum=0;
+    for(ll i=0;i<n;i++)cin>>a[i];
+    sort(a,a+n);
+    for(ll i=0;i<n;i++)sum+=a[i];
     if(sum%360==0)cout<<"YES"<<endl;
     else{
-        int pre[n-1],suf[n-1];
+        ll pre[n-1],suf[n-1];
         pre[0]=a[0];
-        for(int i=1;i<n-1;i++)pre[i]=a[i]+pre[i-1];
+        for(ll i=1;i<n-1;i++)pre[i]=a[i]+pre[i-1];
         suf[n-2]=a[n-1];
-        for(int i=n-2;i>=1;i--)suf[i-1]=a[i]+suf[i];
+        for(ll i=n-2;i>=1;i--)suf[i-1]=a[i]+suf[i];
         bool ok=false;
-        for(int i=0;i<n-1;i++){if(suf[i]==pre[i]){ok=true;break;}}
+        for(ll i=0;i<n-1;i++){if(suf[i]==pre[i]){ok=true;break;}}
         if(ok)cout<<"YES"<<endl;
         else cout<<"NO"<<endl;
+        for(ll i=0;i<n;i++)cout<<pre[i]<<" ";
+        cout<<endl;
+        for(ll i=0;i<n;i++)cout<<suf[i]<<" ";
     }
     return 0;
 }
