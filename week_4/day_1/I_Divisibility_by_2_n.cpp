7b4031d3f7e14a1f246f8b32112938cc7498f2d2
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-22 05:26:20 +0600

    mission accomplished 🔥🚀🔥

--- week_4/day_1/I_Divisibility_by_2_n.cpp
+++ week_4/day_1/I_Divisibility_by_2_n.cpp
@@ -18,11 +18,36 @@ void solve(){
     int a[n];
     for(int i=0;i<n;i++)cin>>a[i];
     int cnt=0;
-    while(n>1){
-        n=n>>1;
-        cnt++;
+    for(int i=0;i<n;i++){
+        int tmp=a[i];
+        while(tmp%2==0){
+            cnt++;tmp/=2;
+        }
     }
-    cout<<cnt<<nl;
+    // cout<<cnt<<nl;
+    if(cnt>=n)c(0);
+    else{
+        int d=n-cnt;
+        int ans=0;
+        vector<int>v;
+        for(int i=1;i<=n;i++){
+            int tmp=i,c=0;
+            while(tmp%2==0){
+                c++;tmp/=2;
+            }
+            v.pub(c);
+        }
+        sort(all(v),greater<int>());
+        for(auto val:v){
+            d-=val;
+            ans++;
+            if(d<=0)break;
+            // cout<<val<<" ";
+        }
+        // cout<<nl;
+        if(d>0)c(-1);
+        else c(ans);
+    }
 }
 int main(){
     FAST;
