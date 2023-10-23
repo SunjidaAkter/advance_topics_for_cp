6b0fcc40ecc60ff12e7980f89c39d15a1f7dd5fb
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-23 19:26:14 +0600

    mission accomplished 🔥🚀🔥

---
+++ week_4/day_3/R_Remove_Prefix.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    int n,cnt=0;cin>>n;
+    vector<int>v(n);
+    map<int,int>mp;
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+        mp[v[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(mp[v[i]]>1){
+            mp[v[i]]--;
+            cnt=i+1;
+        }
+    }
+    c(cnt);
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
