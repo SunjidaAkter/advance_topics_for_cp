807f56280e4e5fe31269c9f67d813fefaed9bbe2
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-30 17:29:14 +0600

    mission accomplished 🔥🚀🔥

--- week_5/day_4/D_T_4.cpp
+++ week_5/day_4/D_T_4.cpp
@@ -1,59 +1,3 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// bool canFillContainers(const vector<int>& capacities, int m, int maxCapacity) {
-//     int containers = 1;
-//     int currentCapacity = 0;
-
-//     for (int i = 0; i < capacities.size(); i++) {
-//         if (currentCapacity + capacities[i] > maxCapacity) {
-//             containers++;
-//             currentCapacity = 0;
-//         }
-
-//         if (containers > m) {
-//             return false;
-//         }
-
-//         currentCapacity += capacities[i];
-//     }
-
-//     return true;
-// }
-
-// int findMinimalMaxCapacity(const vector<int>& capacities, int m) {
-//     int left = *max_element(capacities.begin(), capacities.end());
-//     int right = accumulate(capacities.begin(), capacities.end(), 0);
-//     int result = INT_MAX;
-
-//     while (left <= right) {
-//         int mid = left + (right - left) / 2;
-
-//         if (canFillContainers(capacities, m, mid)) {
-//             result = min(result, mid);
-//             right = mid - 1;
-//         } else {
-//             left = mid + 1;
-//         }
-//     }
-
-//     return result;
-// }
-
-// int main() {
-//     int n, m;
-//     while (cin >> n >> m) {
-//         vector<int> capacities(n);
-//         for (int i = 0; i < n; i++) {
-//             cin >> capacities[i];
-//         }
-
-//         int minimalMaxCapacity = findMinimalMaxCapacity(capacities, m);
-//         cout << minimalMaxCapacity << endl;
-//     }
-
-//     return 0;
-// }
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -69,7 +13,7 @@ bool isValid(int mid,int a[],int n,int m){
         }
         last+=a[i];
     }
-    return (m<=cnt);
+    return (cnt<=m);
 }
 int main(){
     int n,m;
@@ -78,7 +22,7 @@ int main(){
         for(int i=0;i<n;i++)cin>>a[i];
         int ans=INT_MAX,l=0,r=INT_MAX;
         while(l<=r){
-            int mid=l+(l-r)/2;
+            int mid=l+(r-l)/2;
             if(isValid(mid,a,n,m)){
                 ans=min(mid,ans);
                 r=mid-1;
