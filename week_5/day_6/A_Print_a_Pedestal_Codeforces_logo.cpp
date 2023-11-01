63cce16214f2147a40d13d4b48c3edea072b2c2f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-01 12:27:01 +0600

    mission accomplished 🔥🚀🔥

---
+++ week_5/day_6/A_Print_a_Pedestal_Codeforces_logo.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    int n;cin>>n;
+    int first=n/3;
+    int second=first+1;
+    int third=first-1;
+    cout<<first<<" "<<second+(n%3)<<" "<<third<<nl;
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
