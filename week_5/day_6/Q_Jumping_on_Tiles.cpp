565fdb3b4e5d3f0ebfe4f127995bf234a80c1c56
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-01 19:34:13 +0600

    mission accomplished 🔥🚀🔥

--- week_5/day_6/Q_Jumping_on_Tiles.cpp
+++ week_5/day_6/Q_Jumping_on_Tiles.cpp
@@ -13,18 +13,36 @@ using namespace std;
 typedef pair<char,int>pii;
 #define forl(ty,var,str,end) for(ty var=str; var<end; var++)
 # define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+bool cmp(pii a,pii b){
+    if(a.first<b.second){return true;}
+    else if(a.first>b.second)return false;
+    else if(a.first==b.second){
+        if(a.second<b.second)return true;
+        else return false;
+    }
+}
 void solve(){
     string s;cin>>s;
     int n=s.size();
     vector<pii>v;
-    char a,b;
-    if(s[0]>s[n-1]){a=s[n-1];b=s[0];}
+    char a,b;bool ok=false;
+    if(s[0]>s[n-1]){a=s[n-1];b=s[0];ok=true;}
     if(s[0]<s[n-1]){a=s[0];b=s[n-1];}
+    if(s[0]==s[n-1]){a=s[0];b=s[n-1];}
+    //*instead
     for(int i=0;i<n;i++){
-        if(s[i]>=a&&s[i]<=b)v.push_back({s[i],i});
+        if(s[i]>=a&&s[i]<=b)v.push_back({s[i],i+1});
     }
-    sort(v.begin(),v.end());
-    for(int i=0;i<v.size();i++)cout<<v[i].first<<" ";
+    sort(v.begin(),v.end(),cmp);
+    if(ok)reverse(v.begin(),v.end());
+    ll sum=0;
+    for(int i=1;i<v.size();i++){
+        sum+=abs((v[i].first-96)-(v[i-1].first-96));
+    }
+    cout<<abs(a-b)<<" "<<v.size()<<nl;
+    cout<<1<<" ";
+    for(int i=1;i<(v.size()-1);i++)cout<<(v[i].second)<<" ";
+    cout<<s.size();
     cout<<nl;
 }
 int main(){
