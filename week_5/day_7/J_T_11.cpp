e446a9638249e06885a2ec17b34b544884e17f40
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-02 19:55:26 +0600

    mission accomplished 🔥🚀🔥

---
+++ week_5/day_7/J_T_11.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+bool isValid(int mid,int ans,int a[],int n){
+    int cnt=0,last=0;
+    for(int i=0;i<n;i++){
+        cnt+=abs(a[i]-mid);
+    }
+}
+void solve(){
+    int n,x;cin>>n>>x;
+    int a[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+    sort(a,a+n);
+    int l=0,r=INT_MAX;
+    int ans;
+    while(l<=r){
+        int mid=l+(r-l)/2;
+        ll sum=0;
+        for(int i=0;i<n;i++){
+            if(a[i]<=mid)sum+=abs(a[i]-mid);
+        }
+        if(sum<=x){
+            ans=mid;
+            l=mid+1;
+        }else r=mid-1;
+    }
+    c(ans);
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
