c2fcffa5d064570fc8255d86fca2f717fa637bd6
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-05 17:27:47 +0600

    mission accomplished 🔥🚀🔥

---
+++ week_6/day_2/E_Detective_Task.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    string s;cin>>s;
+    if(s.size()==1||s[0]=='0'||s[s.size()-1]=='1')cout<<1<<nl;
+    else{
+
+    }
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
