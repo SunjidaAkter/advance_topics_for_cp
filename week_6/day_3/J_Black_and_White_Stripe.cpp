c6f67ad852e204686434e25af3dd0eac2139d1af
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-07 11:30:41 +0600

    mission accomplished 🔥🚀🔥

--- week_6/day_3/J_Black_and_White_Stripe.cpp
+++ week_6/day_3/J_Black_and_White_Stripe.cpp
@@ -14,57 +14,19 @@ typedef pair<int,int>pii;
 #define forl(ty,var,str,end) for(ty var=str; var<end; var++)
 # define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
 void solve(){
-    int n,k;cin>>n>>k;
+    int n,k,cnt=0,mn=INT_MAX;cin>>n>>k;
     string s;cin>>s;
-    int cnt=0,mx=0,ans=0,x,y;bool ok=false;
+    queue<int>q;
     for(int i=0;i<n;i++){
-        if(s[i]=='B'){
-            cnt++;
-        }
-        if(s[i]=='W'){
-            if(mx<cnt){
-                mx=cnt;
-                x=i-cnt+1;
-                y=(n-1)-x+1;
-            }
-            cnt=0;
+        q.push(s[i]);
+        if(s[i]=='W')cnt++;
+        if(q.size()==k){
+            mn=min(mn,cnt);
+            if(q.front()=='W')cnt--;
+            q.pop();
         }
     }
-    if(mx<cnt){
-        mx=cnt;
-        x=n-1-cnt+1;
-        y=(n-1)-x+1;
-    }
-    if(s=="W"&&k==1)cout<<1<<nl;
-    else if(mx>=k)cout<<0<<nl;
-    else{
-        cnt=0;
-        if(y>=k){
-            for(int i=x;i<n;i++){
-                if(s[i]=='B'){
-                    cnt++;
-                }else{
-                    if(cnt<k){
-                        s[i]='B';
-                        ans++;cnt++;
-                    }else break;
-                }
-            }
-            c(ans);
-        }else{
-            for(int i=x;i>=0;i--){
-                if(s[i]=='B'){
-                    cnt++;
-                }else{
-                    if(cnt<k){
-                        s[i]='B';
-                        ans++;cnt++;
-                    }else break;
-                }
-            }
-            c(ans);
-        }
-    }
+    c(mn);
 }
 int main(){
     FAST;
