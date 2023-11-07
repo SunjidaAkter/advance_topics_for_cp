d02933adf8e657b52e128ce2e6d1af859152d7e1
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-07 22:52:14 +0600

    mission accomplished 🔥🚀🔥

---
+++ week_6/day_4/S_Odd_GCD_Permutation.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    int n;cin>>n;
+    vector<int>v;
+    if(n%2)c(-1);
+    else{
+        for(int i=n/2;i>=1;i--){
+            v.push_back(2*i);
+            v.push_back(2*i-1);
+        }
+        for(auto val:v)cout<<val<<" ";
+        cout<<nl;
+    }
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
