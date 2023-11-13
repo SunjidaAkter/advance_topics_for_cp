a006437c26e92e9a30d15d43c992f72a7f7b6a3a
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-13 17:29:43 +0600

    mission accomplished 🔥🚀🔥

---
+++ week_7/day_3/Z_Subarray_Divisibility.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+typedef pair<ll,ll>pii;
+#define forl(var,str,end) for(ll var=str; var<end; var++)
+int main(){
+    ll n;cin>>n;
+    ll a[n];
+    forl(i,0,n)cin>>a[i];
+    map<ll,ll>mp1;
+    ll sum1=0;
+    ll cnt1=0;
+    mp1[sum1]++;
+    forl(i,0,n){
+        sum1+=a[i];
+        // if(sum1-n)
+        cnt1+=mp1[(sum1-n)%n];
+        // cout<<mp1[(sum1-n)%n]<<" "<<(sum1-n)%n<<endl;
+        mp1[sum1]++;
+    }
+    cout<<cnt1;
+    return 0;
+}
