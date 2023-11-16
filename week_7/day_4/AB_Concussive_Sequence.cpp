98e739794643ef9ff6b0b4382b07ce3380468b1a
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-16 11:35:18 +0600

    mission accomplished 🔥🚀🔥

--- week_7/day_4/AB_Concussive_Sequence.cpp
+++ week_7/day_4/AB_Concussive_Sequence.cpp
@@ -14,50 +14,31 @@ typedef pair<int,int>pii;
 #define forl(ty,var,str,end) for(ty var=str; var<end; var++)
 # define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
 void solve(){
-    int n;cin>>n;
-    vector<int> a(n);
-    for(int i=0;i<n;i++)cin>>a[i];
-    sort(all(a));
-    reverse(all(a));
-    set<int>st;
-    vector<int>v1,v2;
-    if(n&1){
-        for(int i=0;i<(n/2);i++){
-            v1.push_back(a[i]);
-            st.insert(a[i]);
-        }
-        for(int i=n/2;i<n;i++){
-            v2.push_back(a[i]);
-            st.insert(a[i]);
-        }
-    }else{
-        for(int i=0;i<=(n/2);i++){
-            v1.push_back(a[i]);
-            st.insert(a[i]);
-        }
-        for(int i=(n/2)+1;i<n;i++){
-            v2.push_back(a[i]);
-            st.insert(a[i]);
-        }
+    ll n;cin>>n;
+    ll a[n];
+    for(ll i=0;i<n;i++)cin>>a[i];
+    sort(a,a+n);
+    queue<ll>v1;
+    queue<ll>v2;
+    if(n&1)for(ll i=0;i<=(n/2);i++)v1.push(a[i]);
+    else for(ll i=0;i<(n/2);i++)v1.push(a[i]);
+    if(n&1)for(ll i=(n/2)+1;i<n;i++)v2.push(a[i]);
+    else for(ll i=(n/2);i<n;i++)v2.push(a[i]);
+    vector<ll>ans;bool ok=1;
+    while(!v2.empty()){
+        if(v1.front()==v2.front()){ok=0;break;}
+        ans.push_back(v1.front());
+        ans.push_back(v2.front());
+        v1.pop();v2.pop();
     }
-    if(st.size()==1)c(-1);
-    else{
-        bool f=1;
-        vector<int>ans;
-        sort(all(v2));
-        for(int i=0;i<n/2;i++){
-            if(v1[i]==v2[i])f=0;
-            ans.push_back(v2[i]);
-            ans.push_back(v1[i]);
-            // cout<<v1[i]<<" ";
-        }
-        if(v2.size()&1)ans.push_back(v2[v2.size()-1]);
-        if(!f||(ans[ans.size()-2]==v2[v2.size()-1]))c(-1);
-        else{
-            for(int val:ans)cout<<val<<" ";
-        }
-        cout<<nl;
+    if(!v1.empty()&&!ans.empty()){
+        // cout<<v1.front()<<ans.size()<<"h";
+        if(v1.front()==ans[ans.size()-1])ok=0;
+        else ans.push_back(v1.front());
+        v1.pop();
     }
+    if(!ok)c(-1);
+    else{for(ll val:ans)cout<<val<<" ";cout<<nl;}
 }
 int main(){
     FAST;
