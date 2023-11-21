a6da5fcc5fb26d784b307872cb8c9d2f61281725
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-21 17:41:30 +0600

    mission accomplished 🔥🚀🔥

--- super_week_1/day_3/K_Coloring_Rectangles.cpp
+++ super_week_1/day_3/K_Coloring_Rectangles.cpp
@@ -15,50 +15,52 @@ typedef pair<int,int>pii;
 # define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
 void solve(){
      ll m,n,ans=0;cin>>m>>n;
-     if(n==1||m==1){
-          if(n==1){
-               if(m%3==1){
-                    if(m<8)ans=m/2;
-                    else ans=2+(m-4)/3;
-               }
-               if(m%3==2)ans=(m/3)+1;
-               if(m%3==0)ans=(m/3);
-          }
-          if(m==1){
-               if(n%3==1){
-                    if(n<8)ans=n/2;
-                    else ans=2+(n-4)/3;
-               }
-               if(n%3==2)ans=(n/3)+1;
-               if(n%3==0)ans=(n/3);
-          }
-     }else if(n>=m){
-          ll first=3*(n/3)*m;
-          ll first_tiles=first/3;
-          ll second=3*(m/3)*(n%3);
-          ll second_tiles=second/3;
-          if(n%3==0||m%3==0){
-               ans=first_tiles+second_tiles;
-          }else if(n%3==1&&m%3==1){
-               if(m<8)ans=first_tiles+m/2;
-               else ans=first_tiles+2+(m-4)/3;  
-          }else{
-               ans=first_tiles+second_tiles+((n%3)*(m%3)/2);        
-          } 
-     }else{
-          ll first=3*(m/3)*n;
-          ll first_tiles=first/3;
-          ll second=3*(n/3)*(m%3);
-          ll second_tiles=second/3;
-          if(n%3==0||m%3==0){
-               ans=first_tiles+second_tiles;
-          }else if(n%3==1&&m%3==1){
-               if(n<8)ans=first_tiles+n/2;
-               else ans=first_tiles+2+(n-4)/3;  
-          }else{
-               ans=first_tiles+second_tiles+((n%3)*(m%3)/2);        
-          } 
-     }
+     if((m*n)%3==0)ans=m*n/3;
+     else ans=m*n/3+1;
+     // if(n==1||m==1){
+     //      if(n==1){
+     //           if(m%3==1){
+     //                if(m<8)ans=m/2;
+     //                else ans=2+(m-4)/3;
+     //           }
+     //           if(m%3==2)ans=(m/3)+1;
+     //           if(m%3==0)ans=(m/3);
+     //      }
+     //      if(m==1){
+     //           if(n%3==1){
+     //                if(n<8)ans=n/2;
+     //                else ans=2+(n-4)/3;
+     //           }
+     //           if(n%3==2)ans=(n/3)+1;
+     //           if(n%3==0)ans=(n/3);
+     //      }
+     // }else if(n>=m){
+     //      ll first=3*(n/3)*m;
+     //      ll first_tiles=first/3;
+     //      ll second=3*(m/3)*(n%3);
+     //      ll second_tiles=second/3;
+     //      if(n%3==0||m%3==0){
+     //           ans=first_tiles+second_tiles;
+     //      }else if(n%3==1&&m%3==1){
+     //           if(m<8)ans=first_tiles+m/2;
+     //           else ans=first_tiles+2+(m-4)/3;  
+     //      }else{
+     //           ans=first_tiles+second_tiles+((n%3)*(m%3)/2);        
+     //      } 
+     // }else{
+     //      ll first=3*(m/3)*n;
+     //      ll first_tiles=first/3;
+     //      ll second=3*(n/3)*(m%3);
+     //      ll second_tiles=second/3;
+     //      if(n%3==0||m%3==0){
+     //           ans=first_tiles+second_tiles;
+     //      }else if(n%3==1&&m%3==1){
+     //           if(n<8)ans=first_tiles+n/2;
+     //           else ans=first_tiles+2+(n-4)/3;  
+     //      }else{
+     //           ans=first_tiles+second_tiles+((n%3)*(m%3)/2);        
+     //      } 
+     // }
      cout<<ans<<nl;
 }
 int main(){
