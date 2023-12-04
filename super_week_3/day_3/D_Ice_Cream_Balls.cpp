c2511a5e9995e9c539316bb724548751a460c6ea
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-12-04 22:39:00 +0600

    mission accomplished 🔥🚀🔥

---
+++ super_week_3/day_3/D_Ice_Cream_Balls.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    ll n;cin>>n;
+    ll l=1,r=2e9;
+    while(r-l>1){
+        ll mid=l+(r-l)/2;
+        ll mCr=mid*(mid-1)/2;
+        if(mCr<=n)l=mid;
+        else r=mid;
+    }
+    cout<<l+(n-(l*(l-1)/2))<<nl;
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
