64013651b14d7d5aca96d7f782c17f99b6cbd72c
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-12-16 23:56:59 +0600

    mission accomplished 🔥🚀🔥

--- week_8/day_2/C_Sereja_and_Brackets.cpp
+++ week_8/day_2/C_Sereja_and_Brackets.cpp
@@ -12,9 +12,54 @@ class Node{
         open=o;
         close=c;
     }
+    Node(){}
 };
-
+Node tree[3*mx];
+Node merge(Node a,Node b){
+    int ans=min(a.open,b.close);
+    a.open-=ans;
+    b.close-=ans;
+    Node n(a.ans+b.ans+ans,a.open+b.open,a.close+b.close);
+    return n;
+}
+void build(int node,int l,int r,string& s){
+    if(l==r){
+        if(s[l]=='('){
+            tree[node].open=1;
+            tree[node].close=0;
+            tree[node].ans=0;
+        }else{
+            tree[node].close=1;
+            tree[node].open=0;
+            tree[node].ans=0;
+        }
+        return;
+    }
+    int mid=(l+r)/2;
+    int leftNode=2*node;
+    int rightNode=leftNode+1;
+    build(leftNode,l,mid,s);
+    build(rightNode,mid+1,r,s);
+    tree[node]=merge(tree[leftNode],tree[rightNode]);
+}
+Node query(int node,int l,int r,int a,int b){
+    if(a<=l&&r<=b)return tree[node];
+    if(r<a||l>b)return Node(0,0,0);
+    int mid=(l+r)/2;
+    int leftNode=2*node;
+    int rightNode=leftNode+1;
+    return merge(query(leftNode,l,mid,a,b),query(rightNode,mid+1,r,a,b));
+}
 int main(){
-    
+    string s;cin>>s;
+    // s="?"+s;
+    int n;cin>>n;
+    build(1,0,s.size()-1,s);
+    while(n--){
+        int x,y;cin>>x>>y;
+        x--,y--;
+        Node ansNode=query(1,0,s.size()-1,x,y);
+        cout<<ansNode.ans*2<<'\n';
+    }
     return 0;
 }
