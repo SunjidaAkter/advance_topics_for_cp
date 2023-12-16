f8f1b0d49b1507569ca0a1168a57e4aad9acf5f7
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-12-16 20:29:17 +0600

    mission accomplished 🔥🚀🔥

--- week_8/day_2/C_Sereja_and_Brackets.cpp
+++ week_8/day_2/C_Sereja_and_Brackets.cpp
@@ -3,6 +3,17 @@ using namespace std;
 #define ll long long
 typedef pair<ll,ll>pii;
 #define forl(var,str,end) for(long long int var=str; var<end; var++)
+const int mx=1e6+5;
+class Node{
+    public:
+    int ans,open,close;
+    Node(int a,int o,int c){
+        ans=a;
+        open=o;
+        close=c;
+    }
+};
+
 int main(){
     
     return 0;
