387aec3eb13aa240dc34ebde84a79485821d240d
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-12-25 23:19:55 +0600

    mission accomplished 🔥🚀🔥

---
+++ super_week_6/day_3/H_Friendly_Arrays.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    ll n,m,B=0;cin>>n>>m;
+    ll a[n],b[m];
+    for(ll i=0;i<n;i++)cin>>a[i];
+    for(ll i=0;i<m;i++){
+        cin>>b[i];
+        B|=b[i];
+    }
+    ll A=a[0],Al=(B|a[0]);
+    for(ll i=1;i<n;i++){
+        A^=a[i];
+        Al^=(B|a[i]);
+    }
+    if(n%2==0)cout<<Al<<" "<<A<<nl;
+    if(n%2==1)cout<<A<<" "<<Al<<nl;
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
---
+++ super_week_6/day_3/I_Colorful_Table.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    ll n,k;cin>>n>>k;
+    ll a[n+1];
+    set<ll>st;
+    vector<ll>l(k+1,n+1),r(k+1,-1);
+    for(ll i=1;i<=n;i++){
+        cin>>a[i];
+        l[a[i]]=min(l[a[i]],i);
+        r[a[i]]=i;
+        st.insert(a[i]);
+    }
+    for(ll i=k-1;i>=1;i--){
+        l[i]=min(l[i],l[i+1]);
+        r[i]=max(r[i],r[i+1]);
+    }
+    for(ll i=1;i<=k;i++){
+        if(!st.count(i))cout<<0<<" ";
+        else cout<<2*(r[i]-l[i]+1)<<" ";
+    }
+    cout<<nl;
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
