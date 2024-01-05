4127292e7468a7d37394857ef5f8506d7cc868b9
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-05 22:02:51 +0600

    mission accomplished 🔥🚀🔥

--- new_week_1/day_3/E_Segments_with_Small_Set.cpp
+++ new_week_1/day_3/E_Segments_with_Small_Set.cpp
@@ -1,50 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-#define pb push_back
-#define fi first
-#define sec second
-const int N=1000000007;
-
-#define lp for(int i=0;i<n;i++)
-ll f[100001]={0};
-
-int main() { 
-    ios_base::sync_with_stdio(false); cin.tie(NULL); 
-    ll n,k; cin>>n>>k; 
-    ll a[n]; 
-    for(ll i=0;i<n;i++)cin>>a[i]; 
-    int num = 0; 
-    int i=0,prev=0,ans=0;
-    if(k==0){
-        cout<<0;
-    }else{ 
-        while(i<n){ 
-            while(i<n && num<=k){
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,k;cin>>n>>k;
+    ll a[n],f[100001]={0};
+    for(ll i=0;i<n;i++)cin>>a[i];
+    ll i=0,pre=0,ans=0,num=0;
+    if(k==0)cout<<0;
+    else{
+        while(i<n){
+            while(i<n&&num<=k){
                 f[a[i]]++;
                 if(f[a[i]]==1){
                     num++;
                 }
                 if(num<=k){
-                    ans += (i-prev+1);
+                    ans+=(i-pre+1);
                     i++;
                 }else break;
             }
-
             if(i<n){
                 f[a[i]]--;
                 if(f[a[i]]==0)num--;
             }
-
-            f[a[prev]]--;
-            if(f[a[prev]]==0){
-                num--;
-            }
-
-            prev++;
+            f[a[pre]]--;
+            if(f[a[pre]]==0)num--;
+            pre++;
         }
         cout<<ans;
     }
-    return 0; 
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
 }
