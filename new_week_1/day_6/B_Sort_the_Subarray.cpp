032d6097807ddf67c49a83b76a4c86a3d7dcd611
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-08 23:46:36 +0600

    mission accomplished 🔥🚀🔥

---
+++ new_week_1/day_6/B_Sort_the_Subarray.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    ll a[n],b[n],x=0,y=n-1;
+    for(ll i=0;i<n;i++)cin>>a[i];
+    for(ll i=0;i<n;i++)cin>>b[i];
+    ll f,s,f1,s1;
+    for(ll i=0;i<n;i++){
+        if(a[i]!=b[i]){
+            f=i;
+            break;
+        }
+    }
+    for(ll i=n-1;i>=0;i--){
+        if(a[i]!=b[i]){
+            s=i;
+            break;
+        }
+    }
+    for(ll i=f;i>0;i--){
+        if(a[i]>=a[i-1])f1=i-1;
+        
+    }
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
