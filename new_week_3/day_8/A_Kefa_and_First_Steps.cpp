db8a5e5083157009d3ef64c788fe68aca77a789a
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-25 22:52:29 +0600

    mission accomplished 🔥🚀🔥

--- new_week_3/day_8/A_Kefa_and_First_Steps.cpp
+++ new_week_3/day_8/A_Kefa_and_First_Steps.cpp
@@ -10,12 +10,26 @@ using namespace std;
 #define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
 typedef pair<ll,ll>pii;
 void solve(){
-    
+    ll n;cin>>n;
+    ll a[n];
+    for(ll i=0;i<n;i++)cin>>a[i];
+    ll mx=0,cnt=0;
+    for(ll i=1;i<n;i++){
+        if(a[i-1]<=a[i]){
+            cnt++;
+        }else{
+            mx=max(mx,cnt);
+            cnt=0;
+        }
+    }
+    mx=max(mx,cnt);
+    cout<<mx+1<<nl;
 }
 int main(){
     FAST;
     int t=1;
-    cin>>t;
+    // cin>>t;
+    
     while(t--){
         solve();
     }
