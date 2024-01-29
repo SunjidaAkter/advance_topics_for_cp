4dd843d40a5d7d0e3b4cf58e8ba2c175ff16b8a7
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-29 14:09:27 +0600

    mission accomplished 🔥🚀🔥

---
+++ new_week_4/day_4/C_Get_an_Even_String.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    string s;cin>>s;
+    map<char,bool>mp;
+    ll ans=s.size();
+    for(ll i=0;i<s.size();i++){
+        if(mp[s[i]]){
+            ans-=2;
+            mp.clear();
+        }else{
+            mp[s[i]]=true;
+        }
+    }
+    cout<<ans<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
