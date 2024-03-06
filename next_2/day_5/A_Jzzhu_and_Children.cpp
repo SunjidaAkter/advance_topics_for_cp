ba678bf84a9d0da7f4dc6e893de50590c5eb7fd3
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-03-06 19:37:23 +0600

    mission accomplished 🔥🚀🔥

--- next_2/day_5/A_Jzzhu_and_Children.cpp
+++ next_2/day_5/A_Jzzhu_and_Children.cpp
@@ -10,12 +10,31 @@ using namespace std;
 #define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
 typedef pair<ll,ll>pii;
 void solve(){
-    
+    ll n,m;cin>>n>>m;
+    ll a[n];
+    queue<ll>q;
+    map<ll,ll>mp;
+    for(ll i=0;i<n;i++){
+        cin>>a[i];
+        q.push(i);
+    }
+    ll ans=n;
+    while(!q.empty()){
+        ll x=q.front();
+        ans=x+1;
+        if(mp[x]+m>=a[x])q.pop();
+        else{
+            q.pop();
+            mp[x]+=m;
+            q.push(x);
+        }
+    }
+    cout<<ans<<nl;
 }
 int main(){
     FAST;
     int t=1;
-    cin>>t;
+    // cin>>t;
     while(t--){
         solve();
     }
