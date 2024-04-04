25b76f8f6f41fb527fbb1c206a8c72c82d5de872
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-04-04 10:15:21 +0600

    mission accomplished 🔥🚀🔥

---
+++ next_3/day_3/C_They_Have_Fooled.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    ll a[13]={10,10,8,9,8,7,7,7,7,0,9,6,8};
+    cout<<a[n]<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
