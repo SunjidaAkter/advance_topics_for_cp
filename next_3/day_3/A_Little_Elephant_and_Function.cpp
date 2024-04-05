167b826219ce355f57e165360bb0b1810d876fe3
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-04-05 10:10:58 +0600

    mission accomplished 🔥🚀🔥

---
+++ next_3/day_3/A_Little_Elephant_and_Function.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    cout<<n<<" ";
+    for(ll i=1;i<=n-1;i++){
+        cout<<i<<" ";
+    }
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
