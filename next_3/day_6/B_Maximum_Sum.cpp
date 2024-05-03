ba3e4b0f7ed0ed337a8a46203674caa9ef8944e6
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-05-03 10:24:11 +0600

    mission accomplished 🔥🚀🔥

--- next_3/day_6/B_Maximum_Sum.cpp
+++ next_3/day_6/B_Maximum_Sum.cpp
@@ -10,23 +10,7 @@ using namespace std;
 #define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
 typedef pair<ll,ll>pii;
 const ll mod=1e9+7;
-ll maxSubArraySum(ll a[], ll size)
-{
-    ll max_so_far = INT_MIN, max_ending_here = 0;
 
-    for (ll i = 0; i < size; i++) {
-        max_ending_here = (max_ending_here + a[i]);
-        // max_ending_here%=mod;
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
-
-        if (max_ending_here < 0)
-            max_ending_here = 0;
-        if (max_so_far < 0)
-            max_so_far = 0;
-    }
-    return max_so_far;
-}
 void solve(){
     ll n,k;cin>>n>>k;
     ll a[n],sum=0,ans=0;
