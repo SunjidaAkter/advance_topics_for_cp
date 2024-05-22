6ad51c2412db73b11298242205cb61f7a85c01db
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-05-22 15:41:08 +0600

    mission accomplished 🔥🚀🔥

---
+++ next_4/day_6/B_Increase_and_Decrease.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    ll sum=0;
+    ll a[n];
+    for(ll i=0; i<n;i++){
+        cin>>a[i];
+        sum+=a[i];
+    }
+    if(sum%n==0){
+        cout<<n<<nl;
+    }else cout<<n-1<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
