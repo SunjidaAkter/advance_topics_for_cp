003df5cd3a5acec03fe5c4d4bcaacd653115bc94
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-05-31 12:52:33 +0600

    mission accomplished 🔥🚀🔥

---
+++ next_4/day_7/A_Flipping_Game.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin >> n;
+    ll a[n];
+    for(ll i = 0; i < n; i++)cin>>a[i];
+    ll mx=0;
+    for(ll i=0;i<n;i++){
+        for(ll j=0;j<=i;j++){
+            ll cnt=0;
+            for(ll k=0;k<n;k++){
+                if(j<=k&&k<=i)cnt+=!a[k];//if a[k]==0 cnt++;
+                else cnt+=a[k];//if a[k]==1 cnt++;
+            }
+            mx=max(mx,cnt);
+        }
+    }
+    cout<<mx<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
