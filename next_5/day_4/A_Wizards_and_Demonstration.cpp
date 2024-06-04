9d7a4263eff88a6b959589292f7093e6ee48fdad
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-06-04 21:31:39 +0600

    mission accomplished 🔥🚀🔥

---
+++ next_5/day_4/A_Wizards_and_Demonstration.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    double x,y,z;cin>>x>>y>>z;
+    double a=x*z/100;
+    if(ceil(a)-y<0)cout<<0<<nl;
+    else cout<<ceil(a)-y<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
