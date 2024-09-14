9388101ef284f4bd4dc1c8246df0384efe288ef5
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-09-14 18:06:14 +0600

    mission accomplished 🔥🚀🔥

---
+++ next_6/day_5/B_Scale.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,k;cin>>n>>k;
+     ll a[n][n] = { 0 };
+    for (ll i = 0;i < n;i++) {
+        string s;
+        cin >> s;
+        for (ll j = 0;j < n;j++) {
+            a[i][j] = (s[j] - '0');
+        }
+    }
+    for(ll i=0; i<n; i+=k){
+        for(ll j=0; j<n; j+=k){
+            cout<<a[i][j];
+        }
+        cout<<nl;
+    }
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
