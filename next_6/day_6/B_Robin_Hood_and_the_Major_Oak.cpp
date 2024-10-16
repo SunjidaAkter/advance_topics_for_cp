44dce1c253eeb09063b7eb4b55206e9a80f0241f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-10-16 23:25:57 +0600

    mission accomplished 🔥🚀🔥

---
+++ next_6/day_6/B_Robin_Hood_and_the_Major_Oak.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,k;cin>>n>>k;
+    ll x=n-k;
+    ll sum=(n*(n+1)/2)-(x*(x+1)/2);
+    if(sum%2==0)yes;
+    else no;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
