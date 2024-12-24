ff6f3fa4948c3168b5aa92a6f7cf0243962f49fd
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-12-24 23:18:03 +0600

    problem solving challange completed 🔥🚀🔥

---
+++ december/day_24/D_Super_Permutation.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    if(n==1)cout<<1<<nl;
+    else if(n%2)cout<<-1<<nl;
+    else{
+        for(ll i=1; i<n;i+=2){
+            cout<<n-i+1<<" ";
+            cout<<i<<" ";
+        }
+        cout<<nl;
+    }
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
