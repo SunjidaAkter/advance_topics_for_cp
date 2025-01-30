33c5a5c930e049f9df6f4283cb40cc98955d6517
SunjidaAkter <sanjidabintesayid7@gmail.com>
2025-01-30 23:02:49 +0600

    mission accomplished 🔥🚀🔥

---
+++ january/day_30/A_Shape_Perimeter.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,m;cin>>n>>m;
+    ll sum=4*m;
+    if(n==1){
+        ll x,y;cin>>x>>y;
+        cout<<sum<<nl;
+        return;
+    }
+    for(ll i=0;i<n;i++){
+        ll x,y;cin>>x>>y;
+        if(i!=0){
+            sum+=2*(x+y);
+        }
+    }
+    cout<<sum<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
