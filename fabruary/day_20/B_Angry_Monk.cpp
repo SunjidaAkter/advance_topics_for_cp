9f5c66e5fbb30fb23ca3e146887a74c2b2fb7b14
SunjidaAkter <sanjidabintesayid7@gmail.com>
2025-02-20 23:22:32 +0600

    mission accomplished 🔥🚀🔥

---
+++ fabruary/day_20/B_Angry_Monk.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,k;cin>>n>>k;
+    ll a[k];
+    for(int i=0;i<k;i++)cin>>a[i];
+    sort(a,a+k);
+    ll ans=0;
+    for(int i=0;i<k-1;i++){
+        ans+=(a[i]-1)+a[i];
+    }
+    cout<<ans<<endl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
