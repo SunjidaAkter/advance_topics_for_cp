746e0c2fdc6c7392c698a8a00b54eae75d1e6023
SunjidaAkter <sanjidabintesayid7@gmail.com>
2025-04-15 23:08:45 +0600

    mission accomplished 🔥🚀🔥

---
+++ april/unrated_contest_2/B_Lady_Bug.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    string a,b;cin>>a>>b;
+    string s1="0",s2="1";
+    s1+=a,s2+=b;
+    s1+="0",s2+="1";
+    for(ll i=1;i<=n;i++){
+        if(s1[i]=='1'){
+            if(s2[i-1]=='0'||s2[i+1]=='0')continue;
+            else{
+                no;return;
+            }
+        }
+    }
+    yes;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
---
+++ april/unrated_contest_2/B_Moamen_and_k_subarrays.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,k;cin>>n>>k;
+    vector<pii>v(n);
+    for(ll i=0;i<n;i++){
+        cin>>v[i].first;
+        v[i].second=i;
+    }
+    sort(all(v));
+    ll cnt=1;
+    for(ll i=1;i<n;i++){
+        if(v[i].second-v[i-1].second!=1)cnt++;
+    }
+    if(cnt<=k)Yes;
+    else No;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
