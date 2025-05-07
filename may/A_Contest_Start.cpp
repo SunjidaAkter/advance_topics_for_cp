50babfdf283d68f8866863dbe09c320c7240a6cd
SunjidaAkter <sanjidabintesayid7@gmail.com>
2025-05-07 23:13:53 +0600

    mission accomplished 🔥🚀🔥

---
+++ may/A_Contest_Start.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,x,tt;cin>>n>>x>>tt;
+    ll xx=tt/x,cnt=0;
+    if(n<=xx)cnt=((n-1)*n/2); 
+    else cnt=((n-xx)*xx)+((xx-1)*xx/2);
+    cout<<cnt<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
