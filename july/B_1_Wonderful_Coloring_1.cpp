74b80592550f37133573f10ad41a3e0b48741393
SunjidaAkter <sanjidabintesayid7@gmail.com>
2025-07-20 20:06:28 +0600

    mission accomplished 🔥🚀🔥

--- july/B_1_Wonderful_Coloring_1.cpp
+++ july/B_1_Wonderful_Coloring_1.cpp
@@ -10,22 +10,17 @@ using namespace std;
 #define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
 typedef pair<ll,ll>pii;
 void solve(){
-    ll n;cin>>n;
-    bool ok=true;
-    vector<int>v(n);
-    for(ll i=0;i<n;i++)cin>>v[i];
-    for(ll i=1;i<=n;i++){
-        if(v[i-1]!=i)ok=false;
+    string s; cin >> s;
+    map<char,ll>mp;
+    for(char c : s){
+        mp[c]++;
     }
-    if(ok){
-        cout<<0<<nl;
-    }else{
-        if(v[0]==1 || v[n-1]==n){
-            cout<<1<<nl;
-        }else if(v[0]==n && v[n-1]==1){
-            cout<<3<<nl;
-        }else cout<<2<<nl;    
+    ll odd=0, even=0;
+    for(auto it : mp){
+        if(it.second >=2) even++;
+        else odd++;
     }
+    cout<<even+(odd/2)<<nl;
 }
 int main(){
     FAST;
