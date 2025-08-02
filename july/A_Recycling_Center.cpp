2b64de539e22708e5052b1be93a539f174c62dc3
SunjidaAkter <sanjidabintesayid7@gmail.com>
2025-08-02 20:03:49 +0600

    mission accomplished 🔥🚀🔥

--- july/A_Recycling_Center.cpp
+++ july/A_Recycling_Center.cpp
@@ -9,30 +9,37 @@ using namespace std;
 #define No cout<<"No"<<nl
 #define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
 typedef pair<ll,ll>pii;
-void solve(){
 
-    
-    ll n,c;cin>>n>>c;
-    vector<ll>v(n);
-    for(ll i=0;i<n;i++) cin>>v[i];
-    sort(all(v));
-    reverse(all(v));
-    ll cnt=1,sum=0;
-    for(ll i=0;i<n;i++){
-        if(v[i]*cnt<=c){
-            
-            sum++;
-            cnt*=2;
+void solve(){
+    // Step 1: Input নিই
+    int n,c;cin>>n>>c;
+    vector<int>bag_weight(n);
+    for(int i=0;i<n;i++){
+        cin>>bag_weight[i];
+    }
+    // Step 2: Sort descending
+    sort(bag_weight.begin(), bag_weight.end());
+    reverse(bag_weight.begin(), bag_weight.end());
+    // এটি double হওয়ার multiplier
+    int double_cnt=1;
+    // কয়টা bag free তে ধ্বংস করতে পারলাম
+    int destroyed_bag=0;
+    // Step 3: ধ্বংস শুরু (simulation)
+    for(int i=0;i<n;i++){
+        if(bag_weight[i]*double_cnt<=c){
+            destroyed_bag++;// coin লাগলো না
+            double_cnt*=2;// পরের ধাপে double হবে
         }
     }
-    cout<<n-sum<<nl;
+    // Step 4: যেগুলো coin ছাড়া করা গেল না, তাদের সংখ্যাই হলো final উত্তর
+    cout<<n-destroyed_bag<<nl;
 }
 int main(){
     FAST;
-    int t=1;
+    int t=1;//test case count
     cin>>t;
     while(t--){
-        solve();
+        solve();// solve function call
     }
     return 0;
 }
