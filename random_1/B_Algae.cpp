9be1978fe09ba62c26c18c9a13c68c67a48f0648
SunjidaAkter <sanjidabintesayid7@gmail.com>
2025-12-17 22:48:50 +0600

    mission accomplished 🔥🚀🔥

---
+++ random_1/B_Algae.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll r,d,x;cin>>r>>d>>x;
+    for(ll i=1;i<=10;i++){
+        x=r*x-d;
+        cout<<x<<nl;
+    }
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
