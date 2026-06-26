9ca4619ad37dc87d02a273802b07bd3297e9afbc
SunjidaAkter <sanjidabintesayid7@gmail.com>
2026-06-26 18:32:55 +0600

    mission accomplished 🔥🚀🔥

---
+++ random_2/B_Sorted_Adjacent_Differences.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    vector<ll>a(n);
+    for(ll i=0;i<n;i++)cin>>a[i];
+    sort(all(a));
+    vector<ll>v;
+    ll i=0,j=n-1;
+    while(i<=j){
+        v.push_back(a[i]);
+        if(i<j)v.push_back(a[j]);
+        i++,j--;
+    }
+    reverse(all(v));
+    for(ll i=0;i<n;i++)cout<<v[i]<<" ";
+    cout<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
