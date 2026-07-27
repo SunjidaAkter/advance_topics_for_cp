a86726cc0aca16df0a63d1082114ac8ee8982f49
SunjidaAkter <sanjidabintesayid7@gmail.com>
2026-07-27 10:10:36 +0600

    cses problem tasks and deadlines solved 🔥🚀🔥

---
+++ random_3/Tasks_and_Deadlines.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    vector<pii>v(n);
+    vector<pii>a(n);
+    for(ll i=0;i<n;i++){
+        ll x,y;cin>>x>>y;
+        v[i]={x,y};
+        // a[i]={y-x,i};
+    }
+    // sort(v.rbegin(),v.rend());
+    sort(all(v));
+    // reverse(all(v));
+    // for(auto &x:a){
+    //     cout<<x.first<<" "<<x.second<<nl;
+    // }
+    ll sum=0,ans=0;
+    for(ll i=0;i<n;i++){
+        sum+=v[i].first;
+        ans+=v[i].second-sum;
+    }
+    cout<<ans<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
