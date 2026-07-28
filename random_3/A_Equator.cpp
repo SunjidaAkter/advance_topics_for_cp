920a82f7ddd5d9efd365bb89685e6a1714530d65
SunjidaAkter <sanjidabintesayid7@gmail.com>
2026-07-28 22:30:56 +0600

    codeforces problem A_Equator solved 🔥🚀🔥

---
+++ random_3/A_Equator.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    vector<ll>v(n);
+    for(ll i=0;i<n;i++)cin>>v[i];
+    if(n==1){cout<<1<<nl;return;}
+    ll sum=accumulate(all(v),0ll);
+    ll track=(sum/2)+(sum%2);
+    ll cnt=0,i=0;
+    while(cnt<track){
+        cnt+=v[i];
+        i++;
+    }
+    cout<<i<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
